de920434deb0208462c506fccb6dc36219ae04d1
David Wood <david.p.wood@gmail.com>
2015-02-24 23:52:08 +0000

    Began adding console.

---
+++ public/console/js/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('sms-robotics', ['restangular', 'ngRoute']);
+
+app.config(function ($routeProvider, RestangularProvider) {
+  $routeProvider
+    .when('/', { controller: ListCtrl, templateUrl: 'players.html' })
+    .when('/new', { controller: CreateCtrl, templateUrl: 'new.html' })
+    .otherwise({ redirectTo:'/' });
+});
+
+
+function ListCtrl($scope, Restangular) {
+   $scope.players = Restangular.all("players").getList().$object;
+}
+
+function CreateCtrl($scope, $location, Restangular) {
+  $scope.save = function() {
+    Restangular
+      .all('players')
+      .post($scope.player)
+      .then(function (player) {
+        $location.path('/list');
+      });
+  }
+}
